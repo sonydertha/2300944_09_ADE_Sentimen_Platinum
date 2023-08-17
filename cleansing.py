c2bff6171b6df3a7a52c3f22f51fd73a8dfe5f7c
sonydertha <sonyderthas@gmail.com>
2023-08-18 00:12:07 +0700

    rmove repeated char & add notes

--- cleansing.py
+++ cleansing.py
@@ -8,23 +8,29 @@ from db import get_abusive_data, create_connection
 def text_cleansing(text):
     # Bersihkan tanda baca (selain huruf dan angka)
     clean_text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
-    # yg lain
+    # ubah teks menjadi lower case
     clean_text = clean_text.lower()
+    # menghilangkan emoji
     clean_text = re.sub(r'xf0\S+', '', clean_text)
     clean_text = re.sub(r'xe\S+', '', clean_text)
-    clean_text = clean_text.strip()
-    # abusive
-    conn = create_connection()
-    df_abusive = get_abusive_data(conn)
-    abusive_words = df_abusive['word'].tolist()
-    for word in abusive_words:
-        clean_text = clean_text.replace(word, '***')
-    # menggantikan kata sesuai data csv pembeaharuan 1
+    #remove repeated character
+    clean_text = re.sub(r'(.)\\1+', r'\1', clean_text)
+
+    # # abusive  # tidak menggunakkan kamus abusive untuk analisis sentimen
+    # conn = create_connection()
+    # df_abusive = get_abusive_data(conn)
+    # abusive_words = df_abusive['word'].tolist()
+    # for word in abusive_words:
+    #     clean_text = clean_text.replace(word, '***')
+
+    # menggantikan kata alay dengan kata formal
     replacement_words = pd.read_csv('csv_data/new_kamusalay.csv')
     replacement_dict = dict(zip(replacement_words['alay_word'], replacement_words['formal_word']))
     words = clean_text.split()
     replaced_words = [replacement_dict.get(word, word) for word in words]
     clean_text = ' '.join(replaced_words)
+    # menghilangkan spasi di awal dan akhir teks
+    clean_text = clean_text.strip()
     return clean_text
 
 def cleansing_files(file_upload):
